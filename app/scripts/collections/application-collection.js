b993c5bd675bdb6c7bb602d84753da600418e0ca
Paul Curley <paulcurley@pklaptop.hugoandcat.local>
2012-10-22 12:03:12 +0100

    meh

--- app/scripts/collections/application-collection.js
+++
@@ -1,5 +0,0 @@
-test.Collections.ApplicationCollection = Backbone.Collection.extend({
-
-  model: test.Models.ApplicationModel
-
-});
---
+++ app/scripts/router.js
@@ -0,0 +1,24 @@
+
+
+(function ($){
+    'use strict';
+    
+    // Defining the application router, you can attach sub routers here.
+    var Router = Backbone.Router.extend({
+      routes: {
+        "": "index"
+      },
+
+      index: function() {
+
+      }
+    });
+
+    return Router;
+    
+
+}(Zepto));
+
+
+
+ 
